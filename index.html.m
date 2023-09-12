bfb60f0e298253831d8b94023e5cab2c1c0c31e2
negao <141154764+Mafine@users.noreply.github.com>
2023-09-12 11:12:31 -0300

    Add files via upload

---
+++ index.html.m
@@ -0,0 +1,5 @@
+<h1> casa de esportes mafine </h1>
+
+<p>localizado no centro da cidade de sao paulo, a loja que ajuda a achar os seus equipamentos de esportes, tem todos os acessorios de volei, futebol, basquete, e tenis nenha para mafine  </p>
+
+<p>nossa proposito e ajudar vc conhecer o seu esportes e te ajudar a melhorar sua experiencia </p>
